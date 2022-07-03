579e841464ce53f3a07f6f4417691420e0b9acc5
YeabsiraEz <107886988+YeabsiraEz@users.noreply.github.com>
2022-07-03 12:23:22 +0300

    Add files via upload

---
+++ cs223 project 3/cs223 project 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs223_project_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Enter your Physics marks: ");
+                int Marks = int.Parse(Console.ReadLine());
+
+                if (Marks >= 90)
+                {
+                    Console.WriteLine("You got A+ grade. \n\n");
+                }
+                else if (Marks >= 50 && Marks < 90)
+                {
+                    Console.WriteLine("You passed the exam. \n\n");
+                }
+                else
+                {
+                    Console.WriteLine("You failed the exam. \n\n");
+                }
+
+            }
+        }
+
+    }
+}
